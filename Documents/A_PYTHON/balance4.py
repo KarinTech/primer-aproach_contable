808a55e4d66663d2cfc6b02c0ee60065429de74c
Karin Centenaro <centenarokarin7@gmail.com>
2022-12-20 17:54:52 -0300

    first commit agrego codigo a GIT Hub desde VSCode

---
+++ Documents/A_PYTHON/balance4.py
@@ -0,0 +1,23 @@
+mercadoria = input("Enter the amount of Mercadoria: " )
+
+
+
+n=input()
+z=input()
+def balance(n,z):
+      print(balance)
+    if n>z:
+        print ("El balance es negativo" )
+    else:
+        print("El balance es positivo")
+    # TODO: write code...
+
+balance()
+
+
+
+   
+
+
+    
+
